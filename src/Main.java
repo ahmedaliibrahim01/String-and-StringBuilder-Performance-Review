be3d3184e56573c8d73269d765e461ea302b279d
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-02-17 22:51:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n = 10000;
+
+        //1st code block
+        long startTime1 = System.currentTimeMillis();
+        String result1 = "";
+        for (int i = 1; i < n; i++) {
+            result1 += "hello";
+        }
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("1st code block runtime: " + (endTime1-startTime1));
+
+        //2nd code bloc
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder result2 = new StringBuilder();
+        for (int i = 1; i < n; i++) {
+            result2.append("hello");
+        }
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("2nd code block runtime: " + (endTime2-startTime2));
+        }
+    }
